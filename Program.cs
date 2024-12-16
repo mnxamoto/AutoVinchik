05f163c2bcde4a85def56c247e965cca2b286ade
Полковников Михаил <mnxamoto@mail.ru>
2024-12-16 13:42:01 +0300

    Автоматизация, учёт повторов и статистика

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using VkNet;
 using VkNet.Enums.Filters;
 using VkNet.Model;
 
-VkApi vk = new VkApi(new Logger(), captchaSolver: new CaptchaSolverManual());
+VkApi vk = new VkApi(new Logger(), captchaSolver: new CaptchaSolverAuto());
 
 vk.Authorize(new ApiAuthParams
 {
@@ -22,16 +22,32 @@ MessagesGetHistoryParams messagesGetHistoryParams = new MessagesGetHistoryParams
 
 string[] keywords = File.ReadAllLines(@".\keywords.txt");
 
+int[] counts = File.ReadAllLines(@".\statistics.txt").Select(a => Convert.ToInt32(a)).ToArray();
+int count_NoDescription = counts[0];
+int count_NoMatch       = counts[1];
+int count_YesMatch      = counts[2];
+int count_Total         = counts[3];
+int count_Repeat        = counts[4];
+
+List<long?> ids = File.ReadAllLines(@".\ids.txt").Select(a => Convert.ToInt64(a)).Cast<long?>().ToList();
+
 while (true)
 {
-    string text = vk.Messages.GetHistory(messagesGetHistoryParams).Messages.FirstOrDefault().Text;
+    Message message = vk.Messages.GetHistory(messagesGetHistoryParams).Messages.FirstOrDefault();
+    string text = message.Text;
 
     Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine(text);
 
-    string[] lines = text.Split("\n");
+    if ((message.Attachments.Count != 0) && (ids.Contains(message.Attachments[0].Instance.Id)))
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Повтор. Пропускаем.");
+        count_Repeat++;
 
-    if ((text.Contains('\n')) && (keywords.Any(text.Contains)))  //(text.Contains('\n'))" - Пропускаем анкету, если описания нет вообще
+        continue;
+    }
+    else if ((text.Contains('\n')) && (keywords.Any(text.ToLower().Contains)))  //(text.Contains('\n'))" - Пропускаем анкету, если описания нет вообще
     {
         string keyword = "";
 
@@ -45,7 +61,19 @@ while (true)
 
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine($"Ура, есть ключевое слово: {keyword}");
-        Console.ReadKey();
+
+        while (text == vk.Messages.GetHistory(messagesGetHistoryParams).Messages.FirstOrDefault().Text)
+        {
+            Thread.Sleep(1000);
+        }
+
+        count_YesMatch++;
+
+        File.WriteAllLines(@".\ids.txt", ids.Select(a => a.ToString()));
+
+        File.WriteAllLines(
+            @".\statistics.txt", 
+            [$"{count_NoDescription}", $"{count_NoMatch}", $"{count_YesMatch}", $"{count_Total}", $"{count_Repeat}"]);
     }
     else
     {
@@ -60,9 +88,31 @@ while (true)
         vk.Messages.Send(messagesSendParams);
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Нет совпадения по ключевым словам");
+
+        if (!text.Contains('\n'))
+        {
+            count_NoDescription++;
+        }
+        else
+        {
+            count_NoMatch++;
+        }
     }
 
-    Thread.Sleep(300);
+    if (message.Attachments.Count != 0)
+    {
+        ids.Add(message.Attachments[0].Instance.Id);
+    }
+
+    count_Total++;
+    Console.Title = 
+        $"Всего: {count_Total}. " +
+        $"Без описания: {count_NoDescription}. " +
+        $"Нет совпадения по ключевым словам: {count_NoMatch}. " +
+        $"Есть совпадение: {count_YesMatch}. " +
+        $"Повтор: {count_Repeat}.";
+
+    Thread.Sleep(1000);
 }
 
 Console.WriteLine("\r\nВсё");
